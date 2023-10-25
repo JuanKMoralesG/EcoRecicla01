796b8b73d817a2c1ef66e9d1d8a730d8ee13d67b
liancasi <lacaro@ut.edu.co>
2023-10-25 02:55:36 -0500

    commit

--- app/src/main/java/com/example/ecorecicla01/InicioSesion.java
+++ app/src/main/java/com/example/ecorecicla01/InicioSesion.java
@@ -2,13 +2,43 @@ package com.example.ecorecicla01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InicioSesion extends AppCompatActivity {
+
+    TextView registrarse;
+    Button login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+
+        registrarse=findViewById(R.id.textView3);
+        login=findViewById(R.id.button);
+
+        Intent register= new Intent(getApplicationContext(), RegistroReciclaje.class);
+        Intent home= new Intent(getApplicationContext(), Home.class);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(home);
+            }
+        });
+
+        registrarse.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(register);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ecorecicla01/MainActivity.java
+++ app/src/main/java/com/example/ecorecicla01/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.ecorecicla01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bienvenidos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bienvenidos=findViewById(R.id.btn_bienvenidos);
+
+        Intent bienvenida = new Intent(getApplicationContext(),InicioSesion.class);
+
+        bienvenidos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(bienvenida);
+            }
+        });
+
     }
 }
